01e0a78e60dac20ce2cb76c9e1bcc6cda8df1d22
joaopino <joaomiguelpino@gmail.com>
2023-04-16 06:07:49 +0100

    Added Strapi again

---
+++ app/backend/src/api/anuncio/controllers/anuncio.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * anuncio controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::anuncio.anuncio');
